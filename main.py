06515093550a51ea68899af45b092cfa3f760d96
João Matias <joaodamatias@ua.pt>
2022-10-19 19:00:11 +0100

    Template Matching still not working

--- main.py
+++ main.py
@@ -14,9 +14,14 @@ def main():
     #uses a cascade function which will apply different "filters" one by one until it detects an actual face 
     #(algorithm already developed into opencv "Viola-Jones")
 
-    # matias_color = cv.imread("../SaviProject1/Matias.jpg")    #template João Matias
-    # matias = cv.cvtColor(matias_color, cv.COLOR_BGR2GRAY) 
+    matias_color = cv.imread("../SaviProject1/Matias.jpg")    #template João Matias
+    width_height = (800, 600)
+    matias_resized = cv.resize(matias_color, width_height, interpolation= cv.INTER_LINEAR)
 
+    matias_copy = matias_resized.copy()
+    matias = cv.cvtColor(matias_copy, cv.COLOR_BGR2GRAY) 
+
+    tracker = cv.TrackerCSRT_create()
 
     cap = cv.VideoCapture(0)  #open camera video     
     if(cap.isOpened()==False):                 
@@ -31,28 +36,17 @@ def main():
 
     
     face_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml') 
-    
-    counter_detection = 0
-    fixed_iou = 0.7
-    counter_frame = 0
-    # matias_resized = cv.resize(matias, (720,640), interpolation = cv.INTER_AREA)
-    # h,w = matias_resized.shape
-    #cv.imshow('Matias', matias_resized)
-    
-    #print("h= " + str(h) + " w= " + str(w) + " width= " + str(width) + " height= " + str(h))
-
-    #---------------------------------
-    # Execution
-    #---------------------------------
 
     while(cap.isOpened()==True):
 
         ret, cam_vid = cap.read()    # Capture the video frame by frame
-        
+        vid_resized = cv.resize(cam_vid, width_height, interpolation= cv.INTER_LINEAR)
+
         if ret == False:
             break
 
-        vid = cv.flip(cam_vid, 1)                # mirror the video
+        vid = cv.flip(vid_resized, 1)                # mirror the video
+        
         vid_gray = cv.cvtColor(vid, cv.COLOR_BGR2GRAY)   #convert to gray
 
         vid_gui = deepcopy(vid)
@@ -60,25 +54,34 @@ def main():
         #Detects objects of different sizes (faces in this case) in the video and returns them as list of rectangles.
         boxes = face_cascade.detectMultiScale(vid_gray, scaleFactor=1.1, minNeighbors=4, minSize=(150,150))
 
-        detections = []
+
+        timer = cv.getTickCount()
+        fps = cv.getTickFrequency() / (cv.getTickCount() - timer)
 
         for box in boxes:
             x1, y1, w, h = box
-            detection = Detection(x1, y1, w, h, vid_gray, counter_detection)
-            counter_detection += 1
-            detection.draw_detection(vid_gui)
-            detections.append(detection)
-            cv.imshow('detection ' + str(detection.counter), detection.vid)
-    
+            ret = tracker.init(vid, box)
+            ret, box = tracker.update(vid)
+            if ret:
+                p1 = (int(x1), int(y1))
+                p2 = (int(x1 + w), int(y1 + h))
+                cv.rectangle(vid_gui, p1, p2, (255,0,0), 2, 1)
+        
+            match_matias = cv.matchTemplate(vid_gray, matias, cv.TM_CCOEFF_NORMED)   #matches template with video
+            _, max_val, _, max_loc = cv.minMaxLoc(match_matias)
+            cv.rectangle(vid_gui, (max_loc[0], max_loc[1]), (max_loc[0] + w, max_loc[1] + h), (0, 0, 255), 2)
+        
+            cv.putText(vid_gui, "Matias", 
+                        (max_loc[0], max_loc[1]-10),
+                        cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 
+                        2, lineType=cv.LINE_AA)
+
+            print(str(x1) + ' ' +  str(y1) +' ' + str(max_loc[0]) +' ' + str(max_loc[1]))
+        
+        
        
-        # match_matias = cv.matchTemplate(vid_gray, matias, cv.TM_CCOEFF_NORMED)   #matches template with video
-        # _, max_val, _, max_loc = cv.minMaxLoc(match_matias)
 
-
-        # if match_matias is True:
-        #     print('got a match')
-
-        cv.imshow('Camera video', vid_gui)           # Display the resulting frame
+        cv.imshow('Camera video', vid_gui)       # Display the resulting frame
         if cv.waitKey(1) & 0xFF == ord('q'):     # the 'q' button is to quit
             break
     
