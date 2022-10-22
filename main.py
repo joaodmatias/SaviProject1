4985b84df69af1cb1bacb8b8553970d016d0e844
João Matias <joaodamatias@ua.pt>
2022-10-22 20:21:55 +0100

    Face recognition works. Needs tweaking

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from copy import deepcopy
 import numpy  as np
 import cv2 as cv
+import face_recognition
 
 from functions import Detection
 
@@ -11,31 +12,33 @@ def main():
     # Inititalization
     #---------------------------------
     
-    #uses a cascade function which will apply different "filters" one by one until it detects an actual face 
-    #(algorithm already developed into opencv "Viola-Jones")
-
-    matias_color = cv.imread("../SaviProject1/Matias.jpg")    #template João Matias
-    width_height = (800, 600)
-    matias_resized = cv.resize(matias_color, width_height, interpolation= cv.INTER_LINEAR)
-
-    matias_copy = matias_resized.copy()
-    matias = cv.cvtColor(matias_copy, cv.COLOR_BGR2GRAY) 
-
-    tracker = cv.TrackerCSRT_create()
-
     cap = cv.VideoCapture(0)  #open camera video     
     if(cap.isOpened()==False):                 
         print('Error loading media')
-   
-    width  = int(cap.get(3)) 
-    height = int(cap.get(4)) 
-
-    window_name = 'Camera_video'
-    cv.namedWindow(window_name, cv.WINDOW_NORMAL)
-    cv.resizeWindow(window_name, 800, 500)
-
     
-    face_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml') 
+    width_height = (800,600)
+    
+    #Recognizes face templates (do this for adding a person)
+    matias = face_recognition.load_image_file("Matias.jpg")
+    matias_encoding = face_recognition.face_encodings(matias)[0]
+    
+    vicente = face_recognition.load_image_file("Vicente.jpg")
+    vicente_encoding = face_recognition.face_encodings(vicente)[0]
+    
+    our_faces = [matias_encoding, vicente_encoding]
+    our_names = ['Matias', 'Vicente']
+    
+    #Initialize lists
+    face_locations = []
+    face_encodings = []
+    names = []
+    process_this_frame = True
+
+
+    #---------------------------------
+    # Execution
+    #---------------------------------
+
 
     while(cap.isOpened()==True):
 
@@ -45,45 +48,59 @@ def main():
         if ret == False:
             break
 
-        vid = cv.flip(vid_resized, 1)                # mirror the video
-        
-        vid_gray = cv.cvtColor(vid, cv.COLOR_BGR2GRAY)   #convert to gray
+        # mirror the video
+        vid = cv.flip(vid_resized, 1)               
 
         vid_gui = deepcopy(vid)
-
-        #Detects objects of different sizes (faces in this case) in the video and returns them as list of rectangles.
-        boxes = face_cascade.detectMultiScale(vid_gray, scaleFactor=1.1, minNeighbors=4, minSize=(150,150))
-
-
-        timer = cv.getTickCount()
-        fps = cv.getTickFrequency() / (cv.getTickCount() - timer)
-
-        for box in boxes:
-            x1, y1, w, h = box
-            ret = tracker.init(vid, box)
-            ret, box = tracker.update(vid)
-            if ret:
-                p1 = (int(x1), int(y1))
-                p2 = (int(x1 + w), int(y1 + h))
-                cv.rectangle(vid_gui, p1, p2, (255,0,0), 2, 1)
         
-            match_matias = cv.matchTemplate(vid_gray, matias, cv.TM_CCOEFF_NORMED)   #matches template with video
-            _, max_val, _, max_loc = cv.minMaxLoc(match_matias)
-            cv.rectangle(vid_gui, (max_loc[0], max_loc[1]), (max_loc[0] + w, max_loc[1] + h), (0, 0, 255), 2)
-        
-            cv.putText(vid_gui, "Matias", 
-                        (max_loc[0], max_loc[1]-10),
-                        cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 
-                        2, lineType=cv.LINE_AA)
+        if process_this_frame:
+            # Resize frame of video to 1/4 size for faster face recognition processing
+            small_vid = cv.resize(vid, (0, 0), fx=0.25, fy=0.25)
 
-            print(str(x1) + ' ' +  str(y1) +' ' + str(max_loc[0]) +' ' + str(max_loc[1]))
-        
-        
-       
 
-        cv.imshow('Camera video', vid_gui)       # Display the resulting frame
-        if cv.waitKey(1) & 0xFF == ord('q'):     # the 'q' button is to quit
-            break
+            # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+            rgb_small_vid = small_vid[:, :, ::-1]        
+            
+            #Searches faces in current frame
+            face_locations = face_recognition.face_locations(rgb_small_vid)
+            face_encodings = face_recognition.face_encodings(rgb_small_vid, face_locations)
+
+            for face_encoding in face_encodings:
+                # See if the face is a match for the known face(s)
+                found_face = face_recognition.compare_faces(our_faces, face_encoding)
+                name = 'new name'
+                
+                face_distances = face_recognition.face_distance(our_faces, face_encoding)
+                match_id = np.argmin(face_distances)
+                if found_face[match_id]:
+                    name = our_names[match_id]
+                    print(name)
+
+        
+                    for (top, right, bottom, left) in face_locations:
+                        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+                        top *= 4
+                        right *= 4
+                        bottom *= 4
+                        left *= 4
+
+                        # Draw a box around the face
+                        cv.rectangle(vid_gui, (left, top), (right, bottom), (0, 0, 255), 2)
+
+                        # Draw a label with a name below the face
+                        cv.rectangle(vid_gui, (left, bottom - 35), (right, bottom), (0, 0, 255), cv.FILLED)
+                        font = cv.FONT_HERSHEY_DUPLEX
+                        cv.putText(vid_gui, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+                names.append(name)    
+        
+            cv.imshow('Camera video', vid_gui)       # Display the resulting frame
+            if cv.waitKey(1) & 0xFF == ord('q'):     # the 'q' button is to quit
+                break
+        process_this_frame = not process_this_frame
+
+      
+
     
 
     #---------------------------------
@@ -94,9 +111,6 @@ def main():
     cap.release()
     # Destroy all the windows
     cv.destroyAllWindows()
-    
-        
-    cv.waitKey(0)
 
 if __name__ == "__main__":
     main()
